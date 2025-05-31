5143d1aaaf5fac66a63974b3909be9e17efa63e9
imarialu <marialuizaprado13@gmail.com>
2025-05-31 18:44:55 -0300

    praticas do primeiro modulo do curso

--- modulo 1/6.exercicios_interativos/6.verificador_de_número.js
+++ modulo 1/6.exercicios_interativos/6.verificador_de_número.js
@@ -1,4 +1,5 @@
-// Escreva um algoritmo para repetir a leitura de um número enquanto o valor fornecido for diferente de 0. Para cada número fornecido, imprimir se ele é NEGATIVO ou POSITIVO. Quando o número 0 for fornecido a repetição deve ser encerrada sem imprimir mensagem alguma.
+// Escreva um algoritmo para repetir a leitura de um número enquanto o valor fornecido for diferente de 0. Para cada número fornecido, imprimir se ele é NEGATIVO ou POSITIVO. 
+// Quando o número 0 for fornecido a repetição deve ser encerrada sem imprimir mensagem alguma.
 
 const prompt = require('prompt-sync')();
 
