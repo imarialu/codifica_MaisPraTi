77cd9b47977ab247d0cd0b3b88bc7632d565781a
imarialu <marialuizaprado13@gmail.com>
2025-08-07 10:53:25 -0300

    adicionando comentários

--- modulo_3/45.componentes_e_props/src/components/ComponenteClasse.jsx
+++ modulo_3/45.componentes_e_props/src/components/ComponenteClasse.jsx
@@ -1,15 +1,21 @@
 import { Component } from "react";
 
 class ComponenteClasse extends Component{
+    // contrutor recebe as props passadas pelo componente pai
     constructor(props){
-        super(props);
-        this.state = {count: 0};
+        super(props); // Chama o construtor da classe Component (pai)
+        // O super(props) passa as props que a ComponenteClasse (filho) recebeu para o construtor da classe Component (do React)
+        // O React trata e distribui essas props internamente, permitindo que o componente acesse elas via this.props
+        this.state = {count: 0}; // Define o estado inicial com 'count' igual a 0
     }
 
+    // Será chamado assim que o botão for clicado
     incrementCount = () => {
+        // Atualiza o estado de 'count' somando 1 ao valor atual
         this.setState({count: this.state.count + 1});
     }
 
+    // Define o que será exibido no DOM
     render(){
         return(
             <div>
