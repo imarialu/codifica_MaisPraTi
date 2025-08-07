c2ec5bf7e6a6a69aeacf28101f30f4a415c017c1
imarialu <marialuizaprado13@gmail.com>
2025-08-07 20:08:03 -0300

    práticas da aula 46 - ciclo de vida de componentes

---
+++ modulo_3/45.componentes_e_props/src/components/LifeCycleFunctionalComponent.jsx
@@ -0,0 +1,30 @@
+import { useState, useEffect } from "react";
+
+function LifeCycleFunctionalComponent(){
+    const [count, setCount] = useState(0);
+
+    useEffect(() => {
+        console.log("Componente montado!");
+
+        return() => {
+            console.log("Componente será desmontado!");
+        }
+    }, []);
+
+    useEffect(() => {
+        console.log("Componente atualizado!");
+    }, [count]);
+
+    const increment = () => {
+        setCount(count + 1);
+    }
+
+    return(
+        <div>
+            <p>Contagem: {count}</p>
+            <button onClick={increment}>Incrementar</button>
+        </div>
+    );
+}
+
+export default LifeCycleFunctionalComponent;
